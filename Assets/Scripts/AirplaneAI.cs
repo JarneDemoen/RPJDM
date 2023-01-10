e1e15bb5b62230568f1c2898718718ccd21e4226
JarneDemoen <jarne.demoen@student.howest.be>
2023-01-10 20:01:49 +0100

    Have to find an other airplane model + impl physics

--- Assets/Scripts/AirplaneAI.cs
+++ Assets/Scripts/AirplaneAI.cs
@@ -4,15 +4,38 @@ using UnityEngine;
 
 public class AirplaneAI : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] public float speed = 10.0f;
+    [SerializeField] public float rotationSpeed = 10.0f;
+    [SerializeField] public float thrust = 20.0f;
+    [SerializeField] public float lift = 5.0f;
+    [SerializeField] public float drag = 0.1f;
+    [SerializeField] public float minThrottle = 0.0f;
+    [SerializeField] public float maxThrottle = 1000f;
+
+    private float currentThrottle;
+    private Rigidbody rb;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+        currentThrottle = minThrottle;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.Space))
+        {
+            currentThrottle = Mathf.Clamp(currentThrottle + Time.deltaTime, minThrottle, maxThrottle);
+        }
+        else
+        {
+            currentThrottle = Mathf.Clamp(currentThrottle - Time.deltaTime, minThrottle, maxThrottle);
+        }
+
+        // Move the airplane forward
+        rb.AddForce(transform.forward * currentThrottle * thrust);
+
     }
+
+
 }
